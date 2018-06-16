b2c46a32f8662efb1d3cf0110c8f62773c4c1cba
NEWPLAN <enwplan001@163.com>
2018-06-16 16:41:19 +0800

    update

--- base/03_file-transfer/rdma-file-transfer/messages.h
+++ base/03_file-transfer/rdma-file-transfer/messages.h
@@ -6,24 +6,24 @@ const size_t BUFFER_SIZE = 10 * 1024 * 1024;
 
 enum message_id
 {
-  MSG_INVALID = 0,
-  MSG_MR,
-  MSG_READY,
-  MSG_DONE
+	MSG_INVALID = 0,
+	MSG_MR,
+	MSG_READY,
+	MSG_DONE
 };
 
 struct message
 {
-  int id;
+	int id;
 
-  union
-  {
-    struct
-    {
-      uint64_t addr;
-      uint32_t rkey;
-    } mr;
-  } data;
+	union
+	{
+		struct
+		{
+			uint64_t addr;
+			uint32_t rkey;
+		} mr;
+	} data;
 };
 
 #endif
