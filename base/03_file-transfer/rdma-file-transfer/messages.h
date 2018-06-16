1c5e6a2836438bb412476e2594595a2889f43f46
NEWPLAN <enwplan001@163.com>
2018-06-16 16:35:44 +0800

    update

--- base/03_file-transfer/rdma-file-transfer/messages.h
+++ base/03_file-transfer/rdma-file-transfer/messages.h
@@ -2,28 +2,28 @@
 #define RDMA_MESSAGES_H
 
 const char *DEFAULT_PORT = "12345";
-const size_t BUFFER_SIZE = 5 * 1024 * 1024;
+const size_t BUFFER_SIZE = 10 * 1024 * 1024;
 
 enum message_id
 {
-	MSG_INVALID = 0,
-	MSG_MR,
-	MSG_READY,
-	MSG_DONE
+  MSG_INVALID = 0,
+  MSG_MR,
+  MSG_READY,
+  MSG_DONE
 };
 
 struct message
 {
-	int id;
+  int id;
 
-	union
-	{
-		struct
-		{
-			uint64_t addr;
-			uint32_t rkey;
-		} mr;
-	} data;
+  union
+  {
+    struct
+    {
+      uint64_t addr;
+      uint32_t rkey;
+    } mr;
+  } data;
 };
 
 #endif
